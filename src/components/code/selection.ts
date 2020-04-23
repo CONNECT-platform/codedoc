2206676a1e96b607f592c9276517383994742953
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-04-23 11:27:26 +0200

    fix issue with code selection gaps

--- src/components/code/selection.ts
+++ src/components/code/selection.ts
@@ -13,6 +13,7 @@ export function codeSelection() {
           line$.addEventListener('mouseenter', event => {
             if (event.buttons === 1) {
               line$.classList.add('selected');
+              line$.setAttribute('data-just-selected', 'true');
             }
           });
     
@@ -24,11 +25,13 @@ export function codeSelection() {
             }
     
             line$.classList.toggle('selected');
+            if (line$.classList.contains('selected')) line$.setAttribute('data-just-selected', 'true');
           });
     
           line$.addEventListener('click', event => {
             if (!event.shiftKey) {
               line$.classList.remove('selected');
+              line$.removeAttribute('data-just-selected');
               if (!code$.classList.contains('has-selection')) {
                 const copyContent = line$.getAttribute('data-content') || '';
                 if (copyContent.trim().length > 0)
@@ -46,10 +49,43 @@ export function codeSelection() {
                 c$.classList.remove('has-selection');
                 c$.querySelectorAll('.selected').forEach(l$ => l$.classList.remove('selected'));
               });
+
               code$.classList.add('has-selection');
+
+              let start = -1;
+              let end = -1;
+              code$.querySelectorAll('div').forEach((line$, index) => {
+                if (line$.hasAttribute('data-just-selected')) {
+                  if (start === -1) start = end = index;
+                  else end = index;
+                }
+              });
+              code$.querySelectorAll('div').forEach((line$, index) => {
+                if (index >= start && index <= end) {
+                  line$.classList.add('selected');
+                  line$.removeAttribute('data-just-selected');
+                }
+              });
             }
             else code$.classList.remove('has-selection');
           }, 5);
+        });
+
+        code$.addEventListener('mouseleave', () => {
+          let start = -1;
+          let end = -1;
+          code$.querySelectorAll('div').forEach((line$, index) => {
+            if (line$.hasAttribute('data-just-selected')) {
+              if (start === -1) start = end = index;
+              else end = index;
+            }
+          });
+          code$.querySelectorAll('div').forEach((line$, index) => {
+            if (index >= start && index <= end) {
+              line$.classList.add('selected');
+              line$.removeAttribute('data-just-selected');
+            }
+          });
         });
   
         code$.addEventListener('keydown', (ev: any) => {

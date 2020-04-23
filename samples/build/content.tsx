a68a86cc6917d986b81dab80b02e2219d0a41459
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2020-04-23 09:57:05 +0200

    make default state of ToC on desktop configurable

--- samples/build/content.tsx
+++ samples/build/content.tsx
@@ -20,7 +20,7 @@ export function content(_content: HTMLElement, toc: HTMLElement, renderer: Rende
           header={<Header {...config}/>}
           footer={<Footer {...config}/>}
           toc={
-            <ToC search={
+            <ToC default="open" search={
                   config.misc?.github ? 
                   <GithubSearch$
                     repo={config.misc.github.repo} 
--- src/components/page/toc/index.tsx
+++ src/components/page/toc/index.tsx
@@ -7,6 +7,7 @@ import { TocStyle } from './style';
 
 export interface ToCOptions {
   search?: any;
+  default?: 'open' | 'closed';
 }
 
 
@@ -18,6 +19,15 @@ export function ToC(
 ) {
   const classes = this.theme.classes(TocStyle);
   return <div id="-codedoc-toc" class={classes.toc}>
+    {options.default === 'open' ? 
+     <script>{`
+     if (window.matchMedia('(min-width: 1200px)').matches) {
+       if (!localStorage.getItem('-codedoc-toc-active')) {
+         localStorage.setItem('-codedoc-toc-active', "true");
+       }
+     }
+     `}</script>
+    : ''}
     <div class={classes.content}>
       {content}
     </div>

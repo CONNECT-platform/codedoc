2a7df7f6b838523f6001a8c0d4feca10fe59e0a9
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2021-02-13 15:01:34 +0100

    use isolated chalk instance for terminal outputs

--- src/components/code/term-output.ts
+++ src/components/code/term-output.ts
@@ -3,6 +3,7 @@ import chalk from 'chalk';
 
 
 export function terminalOutput(...lines: string[]) {
+  const ctx = new chalk.Instance({ level: 3 });
   const F = Function('chalk', "return chalk`" + lines.join('\n').replace(/\`/g, '\\`') + "`;");
-  return new AU().ansi_to_html(F(chalk));
+  return new AU().ansi_to_html(F(ctx));
 }
